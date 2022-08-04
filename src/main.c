090ddb760ad2846af333fd94dbd249591681415b
Tanguy Chappuis <tchappui@c3r1s5.42lausanne.ch>
2022-08-04 20:00:04 +0200

    main

--- src/main.c
+++ src/main.c
@@ -1,7 +1,20 @@
 #include "../includes/cub3d.h"
 
 // Parse the map and then start the game
-int main()
+int main(int argc, char **argv)
 {
-	game_start();
+	t_data data;
+	t_tex tex;
+
+	if (argc == 2)
+	{
+		parsing(&data, &tex, argv[1]);	
+		game_start();
+		int i = 0;
+		while(data.map[i])
+		{
+			printf("%s\n", data.map[i]);
+			i++;
+		}
+	}
 }
